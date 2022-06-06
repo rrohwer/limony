19adfb8a71aa9a1d2e4e215f60f2ba5982c41164
Robin Rohwer <robin.rohwer@gmail.com>
2022-06-05 17:15:01 -0700

    how to build vignettes added

--- how_to_update_package.R
+++ how_to_update_package.R
@@ -16,9 +16,12 @@ limony <- readRDS("~/Desktop/pop/data/limony-IRD/2021-08-25_processing/5A_taxlis
 usethis::use_data(limony) # can have multiple objects in this call
 
 usethis::use_vignette("introduction")
+devtools::build_vignettes(pkg = "limony")
 
 remove.packages("limony")
 devtools::install_github("rrohwer/limony")
 library(limony)
 data("limony")
+# limony:: # to view all script options
 
+browseVignettes(package = "limony")
